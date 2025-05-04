1d4d6586b9160c38d9c5b335de28d1d2548c0b40
Pedro Lima <pdro.lima@pm.me>
2025-05-03 22:41:26 -0300

    wip

--- packages/belvo-js/tsup.config.ts
+++ packages/belvo-js/tsup.config.ts
@@ -3,7 +3,7 @@ import { name, version } from "./package.json";
 
 export default defineConfig({
   entryPoints: ["./src/**/*.{ts,tsx}"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   outDir: "dist",
   external: ["react", "react-dom","next"],
   bundle: false,
